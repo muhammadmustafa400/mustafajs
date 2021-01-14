b4668003725fb0146b0fd1ae574eb921b4bcdb7d
muhuammadmustafa400 <yaseenhussain400@gmail.com>
2021-01-14 04:14:58 -0800

    complete

---
+++ app.js
@@ -0,0 +1,145 @@
+// var a = prompt("enter your command")
+// var a = a.toUpperCase()
+// alert(a)
+// var a = prompt("enter your command")
+// var a = a.toLowerCase()
+// alert(a)
+
+
+
+// var a = prompt('enter your favourite mobile model number')
+// var b = a.length
+// alert(b)
+// var a = prompt("enter")
+// var b = a.charAt(a.length - 1)
+// alert(b)
+
+
+
+// var userName=prompt("Enter your Input: ");
+
+// if(userName.match(/[\@\.\,\!]/)===null){
+//     alert("Valid Username found");
+    
+// }
+// else{
+// alert("INValid Username found");
+
+// }
+// var a = "the quick brown for jumps over the lazy dog"
+// var b = a.match(/the/gi)
+// alert(b)
+
+
+
+// var a ="pakistani"
+// var b =a.charAt(3)
+// document.write(b)
+
+
+
+// var a = "Hyderabad"
+// var b = a.replace("Hyder","Islam")
+// alert(b) 
+// var a = "Ali and Sami are best friends. They play cricket and football together"
+// var b = a.replace(/and/g,"&" )
+// alert(b)
+
+
+
+
+// var a =  "3.45214"
+// var b = Math.floor(a)
+// alert(b)
+// var a =  "3.45214"
+// var b = Math.round(a)
+// alert(b)
+// var a =  "3.45214"
+// var b = Math.ceil(a)
+// alert(b)
+// var num = parseFloat(prompt("Enter Number"));
+
+// if (num < 0) {
+//     document.write("Your Number is: " + num + "<br>" + "After Round off: " + Math.round(num) + "<br>" + "Floor Value is: " + Math.floor(num) + "<br>" + "Ceil Value is: " + Math.ceil(num))
+// }
+// else{
+//     document.write("You did not write the correct information");
+// }
+
+
+
+
+// var a = Math.random()
+// var b = (a * 6) + 1
+// var c = Math.floor(b)
+// document.write(c)
+// var a = Math.random()
+// var b = (a * 2) + 1
+// var c = Math.floor(b)
+// document.write(c)
+// if(c==1){
+//     alert("head")
+// }
+// else if(c==2){
+//     alert("tail")
+// }
+// var z =+prompt("enter number")
+// var a = Math.random()
+// var b = (a * 10) + 1
+// var c = Math.floor(b)
+// document.write(c)
+// if(z==c){
+//     alert("congratulations you won")
+// }
+// else{
+//     alert("tum har gae")
+// }
+
+
+
+
+
+
+// var weight = prompt("Enter Your Weight");
+
+// if (weight === '50' ){
+//     weight = parseFloat('50');
+//     document.write("Your weight is: " + weight);
+// }
+// else if (weight === '50kgs' ){
+//     weight = parseFloat('50');
+//     document.write("Your Weight is: " + weight );
+// }
+// else if (weight === '50.2kgs' ){
+//     weight = parseFloat('50.2');
+//     document.write("Your Weight is: " + weight );
+// }
+// else if (weight === '50.2kilograms' ){
+//     weight = parseFloat('50.2');
+//     document.write("Your Weight is: " + weight );
+// }
+// else{
+//     alert("Write Correct Information")
+// }
+
+
+
+
+// var a = "472"
+// var b = Number(a)
+// var c = typeof(b)
+// alert(c)
+// var a = "35.6"
+// var b = a .toString()
+// var c =a.replace(".","")
+// alert(c)
+
+
+
+
+
+// var a = +prompt("enter your number")
+// var b = a/240*100
+// var c = b.toFixed(2)
+// alert(c)
+
